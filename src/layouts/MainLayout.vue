5f8c8058ae36a59ade2846355c063261e7ff19b7
rurikv <rurik@stv.ee>
2026-05-23 19:42:38 +0300

    ДЗ №2 для курса OTUS Vue.js.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -69,6 +69,7 @@ function toggleLeftDrawer() {
 // потому что изменений не будет.
 const navLinks = [
   { to: '/', label: 'Главная', icon: 'home' },
+  { to: '/users', label: 'Пользователи', icon: 'people' },
   { to: '/about', label: 'О проекте', icon: 'info' },
   { to: '/vapor', label: 'Vapor Mode', icon: 'speed' },
 ];
